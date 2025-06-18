fb95d31dbb8ad40df1afbbd4c63a5c967baaa8b9
Amit19012014 <Amit19012014@gmail.com>
2025-06-18 10:51:08 +0530

    18 June 2025

--- src/main/java/stepsDefinations/StepDefinations.java
+++ src/main/java/stepsDefinations/StepDefinations.java
@@ -1,35 +1,57 @@
 package stepsDefinations;
 
 import io.cucumber.java.en.*;
+
+import java.time.Duration;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class StepDefinations {
 
-
+    WebDriver driver;
 
     @Given("the user is on the nopCommerce login page")
-    public void the_user_is_on_the_nop_commerce_login_page() {
+    public void navigateToLoginPage() {
+
+        driver=new ChromeDriver();
+        driver.get("https://demo.nopcommerce.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath("//a[normalize-space()='Log in']")).click();
+    }
+
+    @When("the user enters valid credentials \\(username: {string}, password: {string})")
+    public void the_user_enters_valid_credentials_username_password(String user, String pwd) {
+
+        driver.findElement(By.xpath("//input[@id='Email']")).sendKeys(user);
+        driver.findElement(By.xpath("//input[@id='Password']")).sendKeys(pwd);
 
     }
 
-    @When("the user enters valid credentials \\(username : {string} ,password: {string})")
-    public void the_user_enters_valid_credentials_username_password(String username, String password) {
-
-    }
 
     @When("the user clicks on the Login button")
     public void the_user_clicks_on_the_login_button() {
 
+        driver.findElement(By.xpath("//button[normalize-space()='Log in']")).click();
     }
 
-    @Then("the user should be redirected to the My Account Page")
+    @Then("the user should be redirected to the My Account page")
     public void the_user_should_be_redirected_to_the_my_account_page() {
+        boolean status=driver.findElement(By.xpath("//a[@class='ico-account']")).isDisplayed();
+        Assert.assertEquals(status,true);
+    }
+
+    @Then("the user should see a welcome message")
+    public void the_user_should_see_a_welcome_message() {
+        boolean welcometext=driver.findElement(By.xpath("//h2[normalize-space()='Welcome to our store']")).isDisplayed();
+        Assert.assertEquals(welcometext,true);
+        driver.quit();
+    }
 
     }
 
-    @Then("the user should see a welcome page.")
-    public void the_user_should_see_a_welcome_page() {
-
-    }
-}
